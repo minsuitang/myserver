7e267c3b6ee242aebc3017158869683865f8e076
minsuitang <156480732+minsuitang@users.noreply.github.com>
2024-06-23 05:52:47 +0000

    index2

--- index.js
+++ index.js
@@ -2,3 +2,32 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 app.listen(3000);
+
+var example = {
+    "glossary": {
+        "title": "example glossary",
+        "GlossDiv": {
+            "title": "S",
+            "GlossList": {
+                "GlossEntry": {
+                    "ID": "SGML",
+                    "SortAs": "SGML",
+                    "GlossTerm": "Standard Generalized Markup Language",
+                    "Acronym": "SGML",
+                    "Abbrev": "ISO 8879:1986",
+                    "GlossDef": {
+                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
+                        "GlossSeeAlso": ["GML", "XML"]
+                    },
+                    "GlossSee": "markup"
+                }
+            }
+        }
+    }
+};
+app.get(
+    '/api/example.json',
+    function (req, res) {
+        res.send(example);
+    }
+);
